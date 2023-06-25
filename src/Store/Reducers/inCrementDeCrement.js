0448aa66b78c16dc50768a4a01457492cab95523
Koushik <koushik.bag@idiosystech.com>
2023-06-25 21:57:32 +0530

    redux updated

---
+++ src/Store/Reducers/inCrementDeCrement.js
@@ -0,0 +1,11 @@
+//reducer is a pure function
+const initialState = 10;
+
+const changeTheNumber = (state = initialState,action) => {
+    switch (action.type) {
+        case "INCREMENT": return state + 1;
+        case "DECREMENT": return state - 1;
+        default: return state;
+    }
+}
+export default changeTheNumber;
---
+++ src/Store/Reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux'
+import changeTheNumber from '../Reducers/inCrementDeCrement'
+
+const rootReducer = combineReducers({changeTheNumber});
+export default rootReducer;
