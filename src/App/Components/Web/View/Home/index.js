3de0823f53d56ebf283956df07ea69e204e07f2b
Koushik <koushik.bag@idiosystech.com>
2023-06-25 23:52:25 +0530

    increment decrement redux

--- src/App/Components/Web/View/Home/index.js
+++ src/App/Components/Web/View/Home/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { decNumber, incNumber } from '../../../../../Store/Actions';
 
 const Home = () => {
+    const currentValue = useSelector((state) => state.changeTheNumber )
+    const dispatch = useDispatch();
+    const handleOnclick = (action,value) => {
+        if(action === 'INCREMENT'){
+            dispatch(incNumber(value));
+        }else{
+            dispatch(decNumber(value));
+        }
+    };
+
     return (
         <center>
-            <button type="button" class="btn btn-danger">Decrement</button>
-            <input name ="quantity" type='text' value={"0"}/>
-            <button type="button" class="btn btn-primary">Increment</button>
+            <button type="button" className="btn btn-danger" onClick={() => {handleOnclick('DECREMENT',5)}}>Decrement</button>
+            <input name ="quantity" type='text' value={currentValue}/>
+            <button type="button" className="btn btn-primary" onClick={() => {handleOnclick('INCREMENT',5)}}>Increment</button>
         </center>
     )
 }
